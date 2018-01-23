61676949441daa56efdf2f480561aaec032e9313
Zanaca <carlos@zanaca.com>
2018-01-23 14:33:17 -0200

    bugfix: checking if sshuttle

--- conf/Makefile/Darwin_macOS.mk
+++ conf/Makefile/Darwin_macOS.mk
@@ -17,7 +17,7 @@ ifeq ($(shell cat /usr/local/etc/dnsmasq.conf 2> /dev/null || echo no_dnsmasq), 
 	@#sudo launchctl load -w /Library/LaunchDaemons/com.zanaca.dockerdns-dnsmasq.plist
 endif
 	@test jq || brew install `cat requirements.apt | grep net-tools -v` -y 1> /dev/null 1> /dev/null
-	@[ shuttle ] || sudo easy_install sshuttle
+	@which sshuttle || sudo easy_install sshuttle
 	@if [ ! -d /etc/resolver ]; then sudo mkdir /etc/resolver; sudo touch /etc/resolver/$(TLD); fi
 	@echo "nameserver $(IP)" | sudo cat - /etc/resolver/$(TLD) > /tmp/docker-dns-resolv; sudo mv /tmp/docker-dns-resolv /etc/resolver/$(TLD)
 	@sudo sh -c "cat conf/com.zanaca.dockerdns-tunnel.plist | sed s:\{PWD\}:$(PWD):g > /Library/LaunchDaemons/com.zanaca.dockerdns-tunnel.plist"
