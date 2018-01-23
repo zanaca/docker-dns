aa83436f6b6101c9060f7fda26b4480003f196d7
Zanaca <carlos@zanaca.com>
2018-01-23 17:53:00 -0200

    getting enabled interface dynamically and creating .ssh folder if needed

--- conf/Makefile/Darwin_macOS.mk
+++ conf/Makefile/Darwin_macOS.mk
@@ -1,6 +1,6 @@
 OS_VERSION=Macos$(shell sw_vers -productVersion | cut -d. -f1-2)
 LOOPBACK := ''
-IP := $(shell ifconfig en0 | grep "inet " | cut -d\  -f2)
+IP := $(shell ./conf/bin/macos-active/interface.sh)
 DOCKER_CONF_FOLDER := $(HOME)/Library/Containers/com.docker.docker/Data/database/com.docker.driver.amd64-linux/etc/docker
 DNSs := $(shell scutil --dns | grep nameserver | cut -d: -f2 | sort | uniq | sed s/\ //g | sed ':a;N;$!ba;s/\\\n/","/g');
 DNSs := $(shell echo "${DNSs}" | sed s/\ /\",\"/g | sed s/\;//g)
@@ -19,7 +19,7 @@ endif
 	@test jq || brew install `cat requirements.apt | grep net-tools -v` -y 1> /dev/null 1> /dev/null
 	@which sshuttle || sudo easy_install sshuttle
 	@if [ ! -d /etc/resolver ]; then sudo mkdir /etc/resolver; sudo touch /etc/resolver/$(TLD); fi
-	@echo "nameserver $(IP)" | sudo cat - /etc/resolver/$(TLD) > /tmp/docker-dns-resolv; sudo mv /tmp/docker-dns-resolv /etc/resolver/$(TLD)
+	@[ ! -f /etc/resolver/$(TLD) ] && echo "nameserver $(IP)" | sudo cat - /etc/resolver/$(TLD) > /tmp/docker-dns-resolv; sudo mv /tmp/docker-dns-resolv /etc/resolver/$(TLD)
 	@sudo sh -c "cat conf/com.zanaca.dockerdns-tunnel.plist | sed s:\{PWD\}:$(PWD):g > /Library/LaunchDaemons/com.zanaca.dockerdns-tunnel.plist"
 	@echo Loading tunnel service
 	@sudo launchctl load -w /Library/LaunchDaemons/com.zanaca.dockerdns-tunnel.plist 1>/dev/null 2>/dev/null
@@ -33,9 +33,10 @@ install-os:
 	@sed -i '' 's/\s#bind-interfaces//' $(DNSMASQ_LOCAL_CONF)
 	@sed -i '' 's/interface=docker.*//' $(DNSMASQ_LOCAL_CONF)
 	@echo Generating known_hosts backup for user "root", if necessary
+	@test -d $(HOME_ROOT)/.ssh || (mkdir $(HOME_ROOT)/.ssh; chmod 700 $(HOME_ROOT)/.ssh)
 	@if sudo sh -c "[ -e $(HOME_ROOT)/.ssh/known_hosts ]"; then sudo cp $(HOME_ROOT)/.ssh/known_hosts $(HOME_ROOT)/.ssh/known_hosts_pre_hud; fi
 	@echo Adding key to known_hosts for user "root"
-	@sleep 3 && ssh-keyscan -p `docker port $(DOCKER_CONTAINER_NAME) | grep 22/ | cut -d: -f2` 127.0.0.1 | grep ecdsa-sha2-nistp256 | sudo tee -a `echo ~root`/.ssh/known_hosts
+	@sleep 3 && ssh-keyscan -p `docker port $(DOCKER_CONTAINER_NAME) | grep 22/ | cut -d: -f2` 127.0.0.1 | grep ecdsa-sha2-nistp256 | sudo tee -a $(HOME_ROOT)/.ssh/known_hosts
 	@echo Starting tunnel from host machine network to docker network
 	@$(shell test -f macos-tunnel.sh && sudo rm macos-tunnel.sh 1> /dev/null 2> /dev/null)
 	@cat conf/macos-tunnel.sh.tpl | sed s:\{DOCKER_CONTAINER_NAME\}:$(DOCKER_CONTAINER_NAME):g > macos-tunnel.sh;
