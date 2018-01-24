4eb23619d4d267b8c1fba1dbb90d1f672a2291b9
Carlos Rios <carlos@zanaca.com>
2018-01-24 10:01:56 -0200

    removing tests

--- conf/bin/macos-active-ip.sh
+++ conf/bin/macos-active-ip.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-EN=$(./macos-active-interface.sh | head -n1)
-IP=$(ifconfig $EN | grep inet\  | cit -d\  -f2)
+DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+EN=$($DIR/macos-active-interface.sh | head -n1)
+IP=$(ifconfig $EN | grep inet\  | cut -d\  -f2)
 
 echo $IP
-
-echo $1
-echo $2
-exit
 if [[ "$1" == "update-resolver" ]]; then
-    echo "nameserver $IP" | sudo cat - /etc/resolver/$2 > /tmp/docker-dns-resolv; mv /tmp/docker-dns-resolv /etc/resolver/$2
+    echo "nameserver $IP" > /tmp/docker-dns-resolv; mv /tmp/docker-dns-resolv /etc/resolver/$2
 fi
