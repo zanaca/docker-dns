f3dd0bca77b85517cd5241628d963aebc038af1b
Carlos André Sales Rios <carlos@zanaca.com>
2020-10-22 20:40:09 -0300

    wip

--- src/install.py
+++ src/install.py
@@ -12,11 +12,23 @@ def update_cache():
     util.write_cache('name', config.DOCKER_CONTAINER_NAME)
 
 
+def __macos():
+    True
+
+
+def __debian():
+    True
+
+
+def __redhat():
+    True
+
+
 def main(name=config.DOCKER_CONTAINER_NAME, tag=config.DOCKER_CONTAINER_TAG, tld=config.TOP_LEVEL_DOMAIN):
     if os.path.exists('.cache/INSTALLED'):
         os.unlink('.cache/INSTALLED')
     if docker.check_exists(name):
-        print("Stopping existing instance...")
+        print("Stopping existing container...")
         docker.purge(name)
         time.sleep(2)
 
@@ -24,7 +36,7 @@ def main(name=config.DOCKER_CONTAINER_NAME, tag=config.DOCKER_CONTAINER_TAG, tld
         f'Building and starting container "{tag}:latest"')
     docker.build_container(name, tag, tld)
     update_cache()
-    print('built')
+
     print('WIP')
     open('.cache/INSTALLED', 'w').write('')
 
