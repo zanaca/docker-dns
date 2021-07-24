7e9606b9295392b5118b20ddc39b4cfb4767460c
Alexandre Paes <alexandre_slp@poli.ufrj.br>
2021-07-24 04:07:46 -0300

    added step to uninstall resolvconf head file

--- src/OSes/ubuntu.py
+++ src/OSes/ubuntu.py
@@ -7,6 +7,8 @@ FLAVOR = 'ubuntu'
 DOCKER_CONF_FOLDER = '/etc/docker'
 DNSMASQ_LOCAL_CONF = '/etc/NetworkManager/dnsmasq.d/01_docker'
 DOCKER_BUILD_TARGET = 'base'
+RESOLVCONF_HEAD = '/etc/resolvconf/resolv.conf.d/head'
+
 
 if not os.path.exists(DNSMASQ_LOCAL_CONF):
     DNSMASQ_LOCAL_CONF = DNSMASQ_LOCAL_CONF.replace('dnsmasq.d', 'conf.d')
@@ -23,3 +25,8 @@ def install(tld=config.TOP_LEVEL_DOMAIN):
 def uninstall(tld=config.TOP_LEVEL_DOMAIN):
     if os.path.exists(DNSMASQ_LOCAL_CONF):
         os.unlink(DNSMASQ_LOCAL_CONF)
+
+    try:
+        open(RESOLVCONF_HEAD).write('')
+    except FileNotFoundError:
+        pass
