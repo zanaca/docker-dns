cd347dd4edb7b7acc4e60389c572916d0e77d4a1
Alexandre Paes <alexandre_slp@poli.ufrj.br>
2022-05-06 16:38:20 -0300

    refactored how name and version are acquired

--- src/config.py
+++ src/config.py
@@ -4,13 +4,14 @@ import socket
 import platform
 import util
 import json
+import re
 
 
 APP = os.path.basename(sys.argv[0])
-
 USER = os.environ.get('SUDO_USER')
 if not USER:
     USER = os.environ.get('USER')
+
 HOME = os.path.expanduser(f"~{USER}")
 HOME_ROOT = os.path.expanduser("~root")
 BASE_PATH = os.path.dirname(os.path.dirname(__file__))
@@ -18,37 +19,33 @@ HOSTNAME = socket.gethostname()
 HOSTUNAME = platform.uname().system
 
 if util.on_macos or util.on_windows:
-    NAME = platform.uname()[0]
+    NAME = platform.uname()[0].lower()
 
 else:
-    NAME = open('/etc/os-release',
-                'r').read().split('NAME="')[1].split('"')[0]
+    name_pattern = re.compile(r'-(\w*)')
+    NAME = re.search(pattern=name_pattern, string=platform.uname().version).group(1).lower()
 
 if util.on_macos:
-    VERSION_MAJOR_ID = '.'.join(platform.mac_ver()[0].split('.')[0:2])
-    version = platform.mac_ver()[0].split('.')
-    OS_VERSION = int(version[1]) + int(version[0]) * 1000
-elif util.on_windows or util.on_wsl:
-    POWERSHELL_PATH = '/mnt/c/Windows/System32/WindowsPowerShell/v1.0//powershell.exe'
-    VERSION_MAJOR_ID = os.popen(
-        f"{POWERSHELL_PATH} [Environment]::OSVersion.Version.Major").read().split('\n')[0]
-    version = [VERSION_MAJOR_ID, 0]
-else:
-    VERSION_MAJOR_ID = open(
-        '/etc/os-release', 'r').read().split('VERSION_ID="')[1].split('"')[0]
-    version = VERSION_MAJOR_ID.split('.')
+    # OS_VERSION example: '12.0.1'
+    OS_VERSION = platform.mac_ver()[0]
 
-if len(version) > 1:
-    OS_VERSION = int(version[1]) + int(version[0]) * 1000
+elif util.on_windows:
+    # OS_VERSION example: '10.0.19042'
+    OS_VERSION = platform.win32_ver()[1]
+
+elif util.on_wsl:
+    # OS_VERSION example: '10.0.19044.0'
+    powershell_path = '/mnt/c/Windows/System32/WindowsPowerShell/v1.0//powershell.exe'
+    version_path = '[Environment]::OSVersion.VersionString'
+    OS_VERSION = os.popen(f'{powershell_path} {version_path}').read().split(' ')[-1].replace('\n', '')
+
 else:
-    OS_VERSION = int(version[0]) * 1000
-del(version)
+    # OS_VERSION example: '20.04.1'
+    version_pattern = re.compile('~(.*)-')
+    OS_VERSION = re.search(pattern=version_pattern, string=platform.uname().version).group(1)
 
 OS = f'{HOSTUNAME}_{NAME}'
-
 TOP_LEVEL_DOMAIN = (util.read_cache('tld') or 'docker').strip()
 DOCKER_CONTAINER_TAG = (util.read_cache('tag') or 'ns0').strip()
-DOCKER_CONTAINER_NAME = (util.read_cache(
-    'name') or DOCKER_CONTAINER_TAG).strip()
-
+DOCKER_CONTAINER_NAME = (util.read_cache('name') or DOCKER_CONTAINER_TAG).strip()
 SUPPORTED_OSES = json.load(open(f'{BASE_PATH}/supported_os.json', 'r'))
