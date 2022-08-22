bee20164d3053fb4f2e43415d1bf77920c140f81
Alexandre Paes <alexandre_slp@poli.ufrj.br>
2022-08-22 19:35:07 -0300

    fix uninstall folder removal

--- src/OSes/macos.py
+++ src/OSes/macos.py
@@ -85,15 +85,6 @@ def uninstall(tld=config.TOP_LEVEL_DOMAIN) -> int:
 
     if os.path.exists(APP_DESTINATION):
         print('Removing tunnel app')
-        for filename in os.listdir(APP_DESTINATION):
-            file_path = os.path.join(APP_DESTINATION, filename)
-            try:
-                if os.path.isfile(file_path) or os.path.islink(file_path):
-                    os.unlink(file_path)
-                elif os.path.isdir(file_path):
-                    shutil.rmtree(file_path)
-            except Exception as e:
-                print('Failed to delete %s. Reason: %s' % (file_path, e))
-        os.unlink(APP_DESTINATION)
+        shutil.rmtree(APP_DESTINATION)
 
     return 0
